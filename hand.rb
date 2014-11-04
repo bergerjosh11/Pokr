10e93c410db1913bb1e0870f29a47c806f88ff34
bergerjosh11 <bergerjosh11@gmail.com>
2014-11-04 14:47:56 -0500

    first commit

--- hand.rb
+++ hand.rb
@@ -5,39 +5,36 @@ class Hand
    flush? && straight?
   end
 
-  def four_of_a_kind?
-    frequency_map.keys.length do |key|
-      if frequency_map[key] == 4
-        return true
-      end
-    end
-    false
-  end
-
   def full_house?
-    frequency_map.keys.length == 2
+    #frequency_map.keys.length == 2
+    of_a_kind?(3) && of_a_kind?(2)
   end
 
   def flush?
     suits.uniq.count == 1
   end
 
-  def straight?
-    sorted_ranks = ranks.sort
-    if sorted_ranks.uniq.count == 5
-     if sorted_ranks.last - sorted_ranks.first == 4
-      true
-     end
-
+  def straight_range
+    low_card.rank..high_card.rank
   end
 
-  def three_of_a_kind?
-       frequency_map.keys.length do |key|
-      if frequency_map[key] == 3
-        return true
-      end
-    end
-    false
+  def straight?
+    straight_range.to_a ==  cards.sort
+    #sorted_ranks = ranks.sort
+    #if sorted_ranks.uniq.count == 5
+     #if sorted_ranks.last - sorted_ranks.first == 4
+      #true
+     #end
+  end
+
+  def of_a_kind?(number)
+    frequency_map.any? { |rank, group| group.length == number }
+     #frequency_map.keys.length do |key|
+      #if frequency_map[key] == number
+      #  return true
+     # end
+    #end
+    #false
   end
 
   def two_pairs?
@@ -49,25 +46,31 @@ class Hand
   end
 
   def high_card?
-    sorted_hand = hand.sort_by { |card| card.rank }.reverse
-    "High Card: #{sorted_hand[0].to_s}"
+    #sorted_hand = hand.sort_by { |card| card.rank }.reverse
+    #{}"High Card: #{sorted_hand[0].to_s}"
+
+    cards.max_by(&:rank)
   end
+
+  def low_card
+    cards.min_by(&:rank)
 end
 
   private
-  
+
   def frequency_map
-    @frequency_map
-    @frequency_map ||= build_frequency_map
-  end
+    #@frequency_map
+    #@frequency_map ||= build_frequency_map
+  #end
 
-def build_frequency_map
-     hash = {}
+#def build_frequency_map
+    hand.group_by(&:rank)
+    #hash = {}
 
-    ranks.each do |rank|
-      if hash.has_key? rank
-        hash[rank] += 1
-      else
-        hash [rank] = 1
-      end
+    #ranks.each do |rank|
+      #if hash.has_key? rank
+       # hash[rank] += 1
+      #else
+        #hash [rank] = 1
+      #end
 end
--- hand_evaluator.rb
+++ hand_evaluator.rb
@@ -1,105 +1,57 @@
+Dir.glob("hands/*").each do |hand|
+	require "./#{hand}"
+
 class HandEvaluator
-  def initialize(hand)
-    @hand - hand
+Possible_Hands = [
+StraightFlush,
+FourOfAKind,
+FullHouse,
+Flush,
+Straight,
+ThreeOfAKind,
+TwoPair,
+OnePair,
+HighCard,
+]
+
+  def initialize(cards)
+    @cards = cards
   end
 
   def evaluate
-  	ranks = []
-  	suits = []
-  	hand.each do |card|
-  		ranks << card.rank
-  		suits << card.suit
+  	highest_hand = hands.find { |hand| hand.valid? }
+  	highest_hand.to_s
+  	#ranks = []
+  	#suits = []
+  	#hand.each do |card|
+  	#	ranks << card.rank
+  	#	suits << card.suit
     end
 
-    if straight_flush?
+    if StraightFlush.new(cards).valid
     	"Straight Flush"
-    elsif four_of_a_kind?
+    elsif FourOfAKind.new(cards).valid
     	"Four of a Kind"
-    elsif full_house?
+    elsif FullHouse.new(cards).valid
     	"Full House"
-    elsif flush?
+    elsif Flush.new(cards).valid
     	"Flush"
-    elsif straight?
+    elsif Straight.new(cards).valid
     	"straight"
-    elsif three_of_a_kind?
+    elsif ThreeOfAKind.new(cards).valid
 		"Three of a Kind"
-	elsif two_pair?
+	elsif TwoPair.new(cards).valid
 		"Two Pair"
-	elsif one_pair?
+	elsif OnePair.new(cards).valid
 		"One Pair"
-	elsif high_card?
+	elsif HighCard.new(cards).valid
 		"High Card"	    		    
     end
-      def straight_flush?
-   flush? && straight?
   end
-
-  def four_of_a_kind?
-    frequency_map.keys.length do |key|
-      if frequency_map[key] == 4
-        return true
-      end
-    end
-    false
-  end
-
-  def full_house?
-    frequency_map.keys.length == 2
-  end
-
-  def flush?
-    suits.uniq.count == 1
-  end
-
-  def straight?
-    sorted_ranks = ranks.sort
-    if sorted_ranks.uniq.count == 5
-     if sorted_ranks.last - sorted_ranks.first == 4
-      true
-     end
-
-  end
-
-  def three_of_a_kind?
-       frequency_map.keys.length do |key|
-      if frequency_map[key] == 3
-        return true
-      end
-    end
-    false
-  end
-
-  def two_pairs?
-   frequency_map.keys.length == 3
-  end
-
-  def one_pair?
-     frequency_map.keys.length == 4
-  end
-
-  def high_card?
-    sorted_hand = hand.sort_by { |card| card.rank }.reverse
-    "High Card: #{sorted_hand[0].to_s}"
-  end
-end
 
   private
-  
-  def frequency_map
-    @frequency_map
-    @frequency_map ||= build_frequency_map
-  end
 
-def build_frequency_map
-     hash = {}
-
-    ranks.each do |rank|
-      if hash.has_key? rank
-        hash[rank] += 1
-      else
-        hash [rank] = 1
-      end
-end
-
+  def hands
+  	Possible_Hands.map { |hand_class| hand_class.new(cards) }
   end
 end
